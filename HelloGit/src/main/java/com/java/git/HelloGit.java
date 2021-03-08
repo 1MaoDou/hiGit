8b4332f07291b24dea8988fe5ef8352a7724b5bf
Jree <Jree@java.com>
2021-03-08 17:10:55 +0800

    第一次提交

---
+++ HelloGit/src/main/java/com/java/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.java.git;
+
+/**
+ * @Author:Maodou
+ * @Date:2021/3/8 16:51
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("master...");
+    }
+}
